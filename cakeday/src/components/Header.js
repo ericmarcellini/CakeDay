2eacb4f18649590ae0fa6cd976e72471bc60373b
ericmarcellini <delacresta@gmail.com>
2025-01-07 16:13:57 +0100

    New header changes

--- cakeday/src/components/Header.js
+++ cakeday/src/components/Header.js
@@ -11,22 +11,29 @@ const Header = () => {
   
   return (
     <React.Fragment>
-      <img className='logo' src={logo} alt="Logo" />
+      {/* <img className='logo' src={logo} alt="Logo" /> */}
       <header className="header">
         
-        <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
-          <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#gallery">Gallery</a></li>
-            <li><a href="#faq">FAQ</a></li>
-            <li><a href="#inquire">Inquire</a></li>
-          </ul>
-        </nav>
-        <button className="menu-toggle" onClick={toggleMenu}>
+      <nav className="navbar">
+      <ul className="nav-left">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+      <div className="nav-logo">
+        <a href="#home">
+          <img src={logo} alt="Logo" />
+        </a>
+      </div>
+      <ul className="nav-right">
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+        {/* <button className="menu-toggle" onClick={toggleMenu}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
-        </button>
+        </button> */}
       </header>
     </React.Fragment>
   );
