10be2309df6a9c31deb1d801909524118dc7bc12
ericmarcellini <delacresta@gmail.com>
2025-01-14 23:51:11 +0100

    change of gallery stash

--- cakeday/src/components/ImageGallery.js
+++ cakeday/src/components/ImageGallery.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './ImageGallery.css';
 
 // Import images
-import img1 from './images/img1.jpg';
-import img2 from './images/img2.jpg';
-import img3 from './images/img3.jpg';
-import img4 from './images/img4.jpg';
-import img5 from './images/img5.jpg';
-import img6 from './images/img6.jpg';
-import img7 from './images/img7.jpg';
-import img8 from './images/img8.jpg';
-import img9 from './images/img9.jpg';
+import img1 from './images/img1.png';
+import img2 from './images/img2.png';
+import img3 from './images/img3.png';
+import img4 from './images/img4.png';
+import img5 from './images/img5.png';
+import img6 from './images/img6.png';
+import img7 from './images/img7.png';
+import img8 from './images/img8.png';
+import img9 from './images/img9.png';
 
 const images = [
   img1, img2, img3, img4, img5, img6,
